b79a4ff1d23b9d2bdda5296714b0d93c72bd69ac
Mustard <48289475+3Mustard@users.noreply.github.com>
2019-10-06 18:18:46 -0400

    fixed the name and meaning strings.

--- fortune/lib/fortune/scraper.rb
+++ fortune/lib/fortune/scraper.rb
@@ -6,10 +6,10 @@ class Fortune::Scraper
         names = []
         meanings = []
         
-        doc.css("row.is-wrap h2").each {|h2| names << h2.text} 
+        doc.css("row.is-wrap h2").each {|h2| names << h2.text.delete("#") }
         #all the card names are h2 elements except the last one which is an h3 with additional text
-        names << doc.css("row.is-wrap h3").text.split("  ")[0]
-        doc.css("row.is-wrap p").each {|meaning| meanings << meaning.text}
+        names << doc.css("row.is-wrap h3").text.split("  ")[0].delete("#")
+        doc.css("row.is-wrap p").each {|meaning| meanings << meaning.text.split("Learn")[0] }
 
         i = 0
         while i < 22 do
